3a8f2be3180835230d33732f46816eef9346e1cc
abasse05 <abasse882@gmail.com>
2021-07-07 18:29:02 +0000

    Third

--- tchat/admin.py
+++ tchat/admin.py
@@ -2,7 +2,23 @@ from django.contrib import admin
 from .models import Article, Zone
 from .models import Category
 from .models import Contact,Reflecto,Adresse
+from django.contrib.admin import AdminSite
+from django.utils.translation import ugettext_lazy
 
+class MyAdminSite(AdminSite):
+    # Text to put at the end of each page's <title>.
+    site_title = ugettext_lazy('Admin Stockage mesure')
+
+    # Text to put in each page's <h1> (and above login form).
+    site_header = ugettext_lazy('Administration Stockage mesure')
+
+    # Text to put at the top of the admin index page.
+    index_title = ugettext_lazy('Site administration')
+
+admin_site = MyAdminSite()
+
+admin.site.site_header = 'Administration Stockage mesure'
+admin.site.change_list_template = 'admin/base_site.html'
 
 admin.site.register(Article)
 admin.site.register(Category)
